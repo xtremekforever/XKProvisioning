d4ba17d463bc048825f495b8d07a88199b465f14
Jesse L. Zamora <xtremekforever@gmail.com>
2021-03-20 11:09:12 -0400

    Add script to provision Ubuntu 16.04 installation

---
+++ ubuntu-provision-base-16.04.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo ""
+echo "Provision Ubuntu Base Script ---"
+echo "This will install packages and apps that I typically use"
+echo ""
+
+# Upgrade first
+echo "Starting out by updating and upgrading all packages..."
+sudo apt update
+sudo apt upgrade -y
+
+# Essential packages available in the PPA
+echo ""
+echo "Installing essential packages..."
+sudo apt install -y git curl vim zsh htop nload build-essential
+
+# Change default editor to vim
+echo ""
+echo "Changing default editor..."
+sudo update-alternatives --config editor
+
+# Give a chance to change sudo rules
+echo ""
+echo "Opening visudo to change sudo rules..."
+sudo visudo
+
+# Oh-my-zsh
+echo ""
+echo "Installing oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Done!"
